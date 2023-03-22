08813a329be42c6f7d62f14a62eabca4c84b8698
Керим Гучигов <forsyte12316gmail.com@MacBook-Air-Kerim.local>
2023-03-22 14:22:04 +0300

    .

--- finalProject.cs
+++ finalProject.cs
@@ -10,9 +10,26 @@
     }
     return len;
 }
+
+string[] ChangeArr (string[] array, int len)
+{
+    int j = 0;
+    string[] array2 = new string[len];
+    for (int i = 0; i<array.Length; i++)
+    {
+        if (array[i].Length<3 || array[i].Length==3)
+        {
+            array2[j]=array[i];
+            j++;
+        }
+    }
+    return array2;
+}
 System.Console.WriteLine("Введите значения через пробел");
 
 string[] array = Console.ReadLine()!.Split();
 int len  = FillLen(array);
-System.Console.WriteLine(len);
+string[] array2 = ChangeArr(array, len);
+System.Console.WriteLine("["+string.Join(" ", array2)+"]");
+
 
